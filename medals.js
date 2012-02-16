dcd0825bb08e7e411df5e9b08e520d4941e66213
Rob Walsh <brombomb@gmail.com>
2012-02-16 00:05:37 -0700

    Removed debug code

--- medals.js
+++ medals.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     var name = $this.html();
     name = name.replace(/ \(Level \d+\)/, "");
     name = name.replace(/(Apprentice|Master) /, "");
-    console.log(name);
     $this.prepend('<img class="medal" src="medals/' + name.toLowerCase() + '.png">');
   });
 });
